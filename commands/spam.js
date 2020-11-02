2d3c23d0d3653d40843c8e20998dcf9c226d6271
Joel Johnson <jjoel1630@gmail.com>
2020-11-02 14:00:35 -0800

    fixed spam (4)

--- commands/spam.js
+++ commands/spam.js
@@ -6,8 +6,10 @@ module.exports = {
             message.channel.send("How many times u wanna spam bruh");
         }
         var times = args[0];
+        console.log(times);
         if(message.member.hasPermission('MANAGE_MESSAGES')) {
             let msgArgs = args.slice(0).join(" ");
+            console.log(msgArgs);
             for(let i = 0; i < times; i++) {
                 message.channel.send(msgArgs + " " + i);
             }
