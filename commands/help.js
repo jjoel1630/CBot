c51a9039903aa3a160d81a7256a44a9321e4572a
Joel Johnson <jjoel1630@gmail.com>
2020-11-02 12:52:08 -0800

    fixed grammer for help cmd

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports = {
         .setTitle("Commands")
         .addFields(
             { name: "Prefix", value: "$ (Sorry can't change it yet)." },
-            { name: "cls + amount of messages (int: max 100 at a time)", value: "Clears messages but pinned ones" },
+            { name: "cls + amount of messages (int: max 100 at a time)", value: "Clears messages except pinned ones" },
             { name: "echo + what you want to echo", value: "the bot just says whatever you type as arguements" },
             { name: "gg (guessing game) + (int: just don't go crazy)", value: "Basically the int is just how high you want to go. 0 - whatever number" },
             { name: "i + (whatever image you want to search)", value: "Randomly selects and image that you pass in" },
