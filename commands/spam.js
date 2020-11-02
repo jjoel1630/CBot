c853124d6871eecad33d9ece0315e37d7ad1c4e3
Joel Johnson <jjoel1630@gmail.com>
2020-11-02 11:36:26 -0800

    improved spam command

--- commands/spam.js
+++ commands/spam.js
@@ -7,8 +7,9 @@ module.exports = {
             message.channel.send("How many times u wanna spam bruh");
         }
         if(message.member.hasPermission('MANAGE_MESSAGES')) {
+            let msgArgs = args.slice(0).join(" ");
             for(let i = 0; i < args[0]; i++) {
-                message.channel.send("Spamming now " + i);
+                message.channel.send(msgArgs + " " + i);
             }
         }
     }
