13fcb5d67fcc6a07feb96840e104caf2ebe60de2
Joel Johnson <jjoel1630@gmail.com>
2020-11-02 14:15:06 -0800

    fixed spam (5)

--- commands/spam.js
+++ commands/spam.js
@@ -5,14 +5,21 @@ module.exports = {
         if(!args[0]) {
             message.channel.send("How many times u wanna spam bruh");
         }
-        var times = args[0];
-        console.log(times);
-        if(message.member.hasPermission('MANAGE_MESSAGES')) {
-            let msgArgs = args.slice(0).join(" ");
-            console.log(msgArgs);
-            for(let i = 0; i < times; i++) {
-                message.channel.send(msgArgs + " " + i);
+        if(isInteger(args[0])) {
+            var times = args[0];
+            args.shift();
+            if(message.member.hasPermission('MANAGE_MESSAGES')) {
+                let msgArgs = args.slice(0).join(" ");
+                console.log(msgArgs);
+                for(let i = 0; i < times; i++) {
+                    message.channel.send(msgArgs + " " + i);
+                }
+            } else {
+                message.channel.send("lmao you don't have the perms. HAHAHAHAHA.");
+                message.delete({timeout: 100}).catch(console.error);
             }
+        } else {
+            message.channel.send("bro ure first arguement has to be an integer! I would be SMH if i had one.");
         }
     }
 }
