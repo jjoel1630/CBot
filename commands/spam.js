578e21109d41ec390d69cdd1ca25fcda5eff3a4a
Joel Johnson <jjoel1630@gmail.com>
2020-11-02 14:22:32 -0800

    fixed spam (7)

--- commands/spam.js
+++ commands/spam.js
@@ -5,7 +5,7 @@ module.exports = {
         if(!args[0]) {
             message.channel.send("How many times u wanna spam bruh");
         }
-        if(args[0].isInteger()) {
+        if(Number.isInteger(args[0])) {
             var times = args[0];
             args.shift();
             if(message.member.hasPermission('MANAGE_MESSAGES')) {
