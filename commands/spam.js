623d84994519285392bbbc68dfb480c84bc1c588
Joel Johnson <jjoel1630@gmail.com>
2020-11-02 11:41:14 -0800

    fixed spam

--- commands/spam.js
+++ commands/spam.js
@@ -1,4 +1,5 @@
 const { model } = require("mongoose");
+const { del, delete } = require("request");
 module.exports = {
     name : "spam",
     description : "spam",
@@ -6,9 +7,10 @@ module.exports = {
         if(!args[0]) {
             message.channel.send("How many times u wanna spam bruh");
         }
+        var times = args[0];
         if(message.member.hasPermission('MANAGE_MESSAGES')) {
             let msgArgs = args.slice(0).join(" ");
-            for(let i = 0; i < args[0]; i++) {
+            for(let i = 0; i < times; i++) {
                 message.channel.send(msgArgs + " " + i);
             }
         }
