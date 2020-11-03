074a3699b7fdcb4f6e4098c620e23109b279bb82
Joel Johnson <jjoel1630@gmail.com>
2020-11-03 13:25:02 -0800

    fixed pp command

--- commands/pp.js
+++ commands/pp.js
@@ -7,16 +7,30 @@ module.exports = {
 		} else if (!args[0]) {
 			var sizeStr = '8';
 			var size = Math.floor(Math.random() * 15);
-			for (let i = 0; i <= size; i++) {
-				sizeStr = sizeStr + '=';
-				if (i == size) {
-					sizeStr = sizeStr + 'D';
+			if (size === 0) {
+				message.channel.send(
+					`${message.member.user
+						.tag}'s pp is 8D long. Wtf u are either a girl, or your pp is non-existent!`
+				);
+			} else {
+				for (let i = 0; i <= size; i++) {
+					sizeStr = sizeStr + '=';
+					if (i == size) {
+						sizeStr = sizeStr + 'D';
+					}
 				}
+				message.channel.send(
+					`${message.member.user
+						.tag}'s pp is ${sizeStr} long :). Don't ask me how I know.`
+				);
 			}
-			message.channel.send(
-				`${message.member.user.tag}'s pp is ${sizeStr} long :). Don't ask me how I know.`
-			);
-		} else {
+		} else if (
+			args[0] === 'Joel' ||
+			args[0] === 'joel' ||
+			message.mentions.users.first().id === '535671100001222668'
+		) {
+			message.channel.send(`The God's pp is too long to calculate!`);
+		} else if (args[0]) {
 			var sizeStr = '8';
 			var size = Math.floor(Math.random() * 15);
 			for (let i = 0; i <= size; i++) {
