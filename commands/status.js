423463d5f893a2abf74b20094f8f38fedfaf1425
Joel Johnson <jjoel1630@gmail.com>
2020-11-02 18:38:22 -0800

    fixed status cmd?

--- commands/status.js
+++ commands/status.js
@@ -3,7 +3,7 @@ module.exports = {
     description : "set bot status",
     execute(message, args, bot) {
         let msgArgs = args.slice(0).join(" ");
-        bot.user.setPresence({
+        bot.user.setActivity({
             activity: {
                 name: msgArgs,
                 type: 0,
