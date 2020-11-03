8791e9ecbee49d879a5325f731d803b1b0073c2e
Joel Johnson <jjoel1630@gmail.com>
2020-11-03 08:29:36 -0800

    added a secrect command

---
+++ commands/pm.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: 'pm',
+	description: 'pm user',
+	execute(message, args, bot) {
+		message.author.send('yoooo u found a hidden command. You get 500 points!!!! Keep looking for more to get more points.');
+		message.delete({ timeout: 5 }).catch(console.error);
+	}
+};
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const ms = require('ms');
 const fs = require('fs');
-const ytdl = require("ytdl-core");
+const ytdl = require('ytdl-core');
 const cheerio = require('cheerio');
 const request = require('request');
 const Duration = require('humanize-duration');
@@ -16,55 +16,57 @@ const PREFIX = '$';
 
 var servers = {};
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
-for(const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+const commandFiles = fs.readdirSync('./commands/').filter((file) => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
 
-    bot.commands.set(command.name, command);
+	bot.commands.set(command.name, command);
 }
 
 bot.on('ready', () => {
-    console.log('ACTIVE!')
+	console.log('ACTIVE!');
 });
 
-bot.on('message', message=>{
-    if(!message.content.startsWith(PREFIX) || message.author.bot) return;
-    
-    let args = message.content.substring(PREFIX.length).split(" ");
-    const command = args.shift().toLowerCase();
+bot.on('message', (message) => {
+	if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 
-    if(command === 'ping') { 
-       bot.commands.get('ping').execute(message, args);
-    } else if(command === 'g') {
-        bot.commands.get('g').execute(message, args);
-    } else if(command === 'cls') {
-        bot.commands.get('cls').execute(message, args);
-    } else if(command === 'pl') {
-        bot.commands.get('pl').execute(message, args);
-    } else if(command === 'welcome') {
-        bot.commands.get('welcome').execute(message, args);
-    } else if(command === 'poll') {
-        bot.commands.get('poll').execute(message, args, Embed);
-    } else if(command === 'i') {
-        bot.commands.get('i').execute(message, args, cheerio, request);
-    } else if(command === 'pick') {
-        bot.commands.get('pick').execute(message, args, bot, Discord);
-    } else if(command === 'members') {
-        bot.guilds.cache.forEach((guild) =>{
-            message.channel.send(`${guild.name} has a total of ${guild.memberCount} members.`)
-        })
-        message.delete({timeout: 50}).catch(console.error);
-    } else if(command === 'gg') { 
-        bot.commands.get('gg').execute(message, args, bot, Duration);
-    } else if(command === 'spam') {
-        bot.commands.get('spam').execute(message, args, bot);
-    } else if(command === 'echo') {
-        bot.commands.get('echo').execute(message, args, bot);
-    } else if(command === 'help') {
-        bot.commands.get('help').execute(message, args, Discord);
-    } else if(command === 'status') {
-        bot.commands.get('status').execute(message, args, bot);
-    }
+	let args = message.content.substring(PREFIX.length).split(' ');
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		bot.commands.get('ping').execute(message, args);
+	} else if (command === 'g') {
+		bot.commands.get('g').execute(message, args);
+	} else if (command === 'cls') {
+		bot.commands.get('cls').execute(message, args);
+	} else if (command === 'pl') {
+		bot.commands.get('pl').execute(message, args);
+	} else if (command === 'welcome') {
+		bot.commands.get('welcome').execute(message, args);
+	} else if (command === 'poll') {
+		bot.commands.get('poll').execute(message, args, Embed);
+	} else if (command === 'i') {
+		bot.commands.get('i').execute(message, args, cheerio, request);
+	} else if (command === 'pick') {
+		bot.commands.get('pick').execute(message, args, bot, Discord);
+	} else if (command === 'members') {
+		bot.guilds.cache.forEach((guild) => {
+			message.channel.send(`${guild.name} has a total of ${guild.memberCount} members.`);
+		});
+		message.delete({ timeout: 50 }).catch(console.error);
+	} else if (command === 'gg') {
+		bot.commands.get('gg').execute(message, args, bot, Duration);
+	} else if (command === 'spam') {
+		bot.commands.get('spam').execute(message, args, bot);
+	} else if (command === 'echo') {
+		bot.commands.get('echo').execute(message, args, bot);
+	} else if (command === 'help') {
+		bot.commands.get('help').execute(message, args, Discord);
+	} else if (command === 'status') {
+		bot.commands.get('status').execute(message, args, bot);
+	} else if (command === 'pm') {
+		bot.commands.get('pm').execute(message, args, bot);
+	}
 });
 
 bot.login(config.token);
