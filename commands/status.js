b281b5c87994409f3aa0e27e2b953495735e745c
Joel Johnson <jjoel1630@gmail.com>
2020-11-03 14:44:07 -0800

    added restrictions to status cmd

--- commands/status.js
+++ commands/status.js
@@ -1,18 +1,25 @@
 module.exports = {
-    name : "status",
-    description : "set bot status",
-    execute(message, args, bot) {
-        console.log(bot.users.size);
-        if(args[0] === "watching") {
-            bot.user.setActivity(`over many people and ${bot.guilds.cache.size} servers`, { type: 'WATCHING' });
-        } else {
-            var msgArgs = args.slice(0).join(" ");
-            bot.user.setPresence({
-                activity: {
-                    name: msgArgs,
-                    type: 0,
-                },
-            })
-        }
-    }
-}
+	name: 'status',
+	description: 'set bot status',
+	execute(message, args, bot) {
+		console.log(bot.users.size);
+		q;
+		if (message.user.id == '535671100001222668') {
+			if (args[0] === 'watching') {
+				bot.user.setActivity(`over many people and ${bot.guilds.cache.size} servers`, {
+					type: 'WATCHING'
+				});
+			} else {
+				var msgArgs = args.slice(0).join(' ');
+				bot.user.setPresence({
+					activity: {
+						name: msgArgs,
+						type: 0
+					}
+				});
+			}
+		} else {
+			message.channel.send('Are you the owner of this bot? No.');
+		}
+	}
+};
