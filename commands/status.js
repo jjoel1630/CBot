6cb18ec0c8648373822e745936897c6f96e3b820
Joel Johnson <jjoel1630@gmail.com>
2020-11-02 18:54:29 -0800

    updated status cmd (2)

--- commands/status.js
+++ commands/status.js
@@ -2,11 +2,11 @@ module.exports = {
     name : "status",
     description : "set bot status",
     execute(message, args, bot) {
-        let msgArgs = args.slice(0).join(" ");
         console.log(msgArgs)
         if(args[0] === "watching") {
             bot.user.setPresence(`Watching over ${bot.users.size} people`);
         } else {
+            let msgArgs = args.slice(0).join(" ");
             bot.user.setPresence({
                 activity: {
                     name: msgArgs,
