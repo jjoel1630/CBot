9a43edd03b738a1e2c6a69fc7306e02566562dad
Joel Johnson <jjoel1630@gmail.com>
2020-11-02 19:10:13 -0800

    updated status cms (9)

--- commands/status.js
+++ commands/status.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message, args, bot) {
         console.log(bot.users.size);
         if(args[0] === "watching") {
-            bot.user.setActivity(`over servers and ${bot.guilds.cache.size} servers`, { type: 'WATCHING' });
+            bot.user.setActivity(`over many people and ${bot.guilds.cache.size} servers`, { type: 'WATCHING' });
         } else {
             var msgArgs = args.slice(0).join(" ");
             bot.user.setPresence({
