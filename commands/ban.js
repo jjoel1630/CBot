7105b1348d5afd3f5ab0c0dc70a29905ddf53a5d
Joel Johnson <jjoel1630@gmail.com>
2020-11-06 07:25:11 -0800

    fixed ban and kick cms (4)

--- commands/ban.js
+++ commands/ban.js
@@ -9,7 +9,7 @@ module.exports = {
 				const targetMember = message.guild.members.cache.get(target.id);
 				targetMember.ban();
 				message.channel.send(
-					`<@${message.member.user.tag}>, has been banned lmaoooooo. Get yo butt outta here.`
+					`${args[0]}, has been banned lmaoooooo. Get yo butt outta here.`
 				);
 			} else {
 				message.channel.send(
