69907ff07dd40ed00489f8caa77d00a40097a1c5
Joel Johnson <jjoel1630@gmail.com>
2021-02-09 12:20:35 -0800

    fixed snipe cmd 6

--- commands/snipe.js
+++ commands/snipe.js
@@ -3,11 +3,11 @@ module.exports = {
     description: 'gets last deleted msg',
     aliases: ['snipe'],
     execute(message=message, args=args, bot=bot, deletedMsg=deletedMsg) {
-        console.log(deletedMsg);
-        message = deletedMsg.get('deleted msg').message;
-        author = deletedMsg.get('deleted msg').author;
-        createdAt = deletedMsg.get('deleted msg').created;
+        console.log(typeof(deletedMsg));
+        // message = deletedMsg.get('deleted msg').message;
+        // author = deletedMsg.get('deleted msg').author;
+        // createdAt = deletedMsg.get('deleted msg').created;
 
-        message.channel.send(`message: ${message}, author: ${author}, createdAt: ${createdAt}`);
+        // message.channel.send(`message: ${message}, author: ${author}, createdAt: ${createdAt}`);
     }
 }
