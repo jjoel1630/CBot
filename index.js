1f557fbb8f9d8b150bdc99da747d0a30f5082d4e
Joel Johnson <jjoel1630@gmail.com>
2021-02-10 09:47:31 -0800

    i think i fixed the snipe cmd

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ bot.on('message', message => {
 
 bot.on("messageDelete", (message) => {
 	if (message.author.bot) return;
-	console.log(message);
+	deletedMsg.delete('deleted msg');
 	deletedMsg.set("deleted msg", {'content': message.content, 'author': message.author.tag, 'created': message.createdAt});
 });
 
