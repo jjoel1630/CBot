4c41f987e6d415130c93a1c4b7ff515e3fc4ed82
Joel Johnson <jjoel1630@gmail.com>
2021-02-10 10:10:21 -0800

    tryan fix snipe cmd

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ bot.on('message', message => {
 		let args = message.content.substring(config.prefix.length).split(' ');
 		const cmd = args.shift().toLowerCase();
 
-		if(cmd == 'snipe' && deletedMsg.get("delted msg") != undefined) {
+		if(cmd == 'snipe') {
 			dmessage = deletedMsg.get('deleted msg').dcontent;
 			dauthor = deletedMsg.get('deleted msg').person;
 			dcreated = deletedMsg.get('deleted msg').created;
