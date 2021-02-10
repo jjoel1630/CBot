a4514ace63018f01ef84964ba9f35c3db3a05f09
Joel Johnson <jjoel1630@gmail.com>
2021-02-10 09:13:07 -0800

    testing news cmd 1

--- commands/news.js
+++ commands/news.js
@@ -8,34 +8,32 @@ module.exports = {
     execute(message=message, args=args, bot=bot) {
         var request = new XMLHttpRequest();
 
-        message.channel.send(args);
+        key = '&apiKey=1b28b79af51a4d28816452334d1dee0f'
+        params = '?country=us&category=business'
+        api = 'https://newsapi.org/v2/top-headlines'
 
-        // key = '&apiKey=1b28b79af51a4d28816452334d1dee0f'
-        // params = '?country=us&category=business'
-        // api = 'https://newsapi.org/v2/top-headlines'
+        call = api + params + key
 
-        // call = api + params + key
+        request.open('GET', call, true)
+        request.onload = function () {
+            var data = JSON.parse(this.responseText);
 
-        // request.open('GET', call, true)
-        // request.onload = function () {
-        //     var data = JSON.parse(this.responseText);
+            articleNum = Math.floor(Math.random() * 10)
 
-        //     articleNum = Math.floor(Math.random() * 10)
+            var article = JSON.stringify(data.articles[articleNum], null, 4)
 
-        //     var article = JSON.stringify(data.articles[articleNum], null, 4)
+            // fs.writeFile('news.txt', article, function(err) {
+            //     if(err) {
+            //         return console.error(err);
+            //     }
+            // })
 
-        //     fs.writeFile('news.txt', article, function(err) {
-        //         if(err) {
-        //             return console.error(err);
-        //         }
-        //     })
-
-        //     // if(request.status >= 200 && request.status < 400) {
-        //     //     console.log(articles[20]);
-        //     // } else {
-        //     //     console.log('error getting request');
-        //     // }
-        // }
-        // request.send()
+            if(request.status >= 200 && request.status < 400) {
+                message.channel.send(article);
+            } else {
+                console.log('error getting request');
+            }
+        }
+        request.send()
     }
 }
