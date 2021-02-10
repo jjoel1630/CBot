0b9bf46fa63f48a0d79cad7ec8352375c61a1094
Joel Johnson <jjoel1630@gmail.com>
2021-02-10 09:23:10 -0800

    trying out new cmd 3

--- commands/news.js
+++ commands/news.js
@@ -8,15 +8,24 @@ module.exports = {
     execute(message=message, args=args, bot=bot) {
         var request = new XMLHttpRequest();
 
-        key = '&apiKey=1b28b79af51a4d28816452334d1dee0f'
-        params = `?country=${args[0]}&category=${args[1]}`
-        api = `https://newsapi.org/v2/top-headlines`
+        if(!args[1]) {
+            message.channel.send('$news <country ex. us, ru, ch> <category ex. health, business>');
+        }
+
+        key = '&apiKey=1b28b79af51a4d28816452334d1dee0f';
+        params = `?country=${args[0]}&category=${args[1]}`;
+        api = `https://newsapi.org/v2/top-headlines`;
 
         call = api + params + key
 
         request.open('GET', call, true)
         request.onload = function () {
             var data = JSON.parse(this.responseText);
+
+            if(!data) {
+                message.channel.send('$news <country ex. us, ru, ch> <category ex. health, business> invaild country or category');
+                return;
+            }
 
             articleNum = Math.floor(Math.random() * 10)
 
