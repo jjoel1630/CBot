7ae630d65e1056280d99dea041fb6ac28c825a9d
Joel Johnson <jjoel1630@gmail.com>
2021-02-10 08:59:23 -0800

    trying to fix bot

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const request = require('request');
 const Duration = require('humanize-duration');
 const config = require('./config.json');
 const help = require('./commands/help');
-const { clearScreenDown } = require('readline');
-
 
 //clients
 const bot = new Discord.Client();
@@ -62,7 +60,6 @@ bot.on('message', message => {
 			.setThumbnail(message.author.avatarURL());
 			message.channel.send(DEmbed);
 		} else {
-			console.log(message);
 			if(bot.aliases.get(cmd)) {
 				const command = bot.commands.get(bot.aliases.get(cmd));
 				command.execute(message, args, bot, Discord, Duration, cheerio); 
