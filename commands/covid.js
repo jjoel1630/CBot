feccf802021863428f03fbda0fa0ddc687ac8550
Joel Johnson <jjoel1630@gmail.com>
2021-02-11 13:52:15 -0800

    testing covid cmd 19

--- commands/covid.js
+++ commands/covid.js
@@ -15,6 +15,8 @@ module.exports = {
             api = 'https://api.covid19api.com/summary'
         } else if(args[0] && args[0] === 'countries') {
             api = 'https://api.covid19api.com/countries'
+        } else if(args[0] && args[0].toLowerCase() === 'country') {
+            api = `https://api.covid19api.com/summary`
         }
 
         request.open('GET', api, true)
@@ -50,11 +52,12 @@ module.exports = {
                 .setThumbnail('https://ewscripps.brightspotcdn.com/dims4/default/7671677/2147483647/strip/true/crop/1303x733+15+0/resize/1280x720!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F0a%2Ff2%2F72b1b4d94794992a0772cb593ce5%2Fscreen-shot-2020-02-25-at-10.49.27%20AM.png');
 
                 message.channel.send(Globalembed);
-            }else if(args[0] && args[0].toLowerCase() === 'countries') {
+            } else if(args[0] && args[0].toLowerCase() === 'countries') {
                 for(let x = 0; x < data.length; x++) {
                     countries = countries + `\`${data[x].Slug}\` `
                 }
                 var countriesList = countries.split(' ');
+                countriesList.sort();
                 var splitIndex = Math.floor(countriesList.length / 2)
                 var countriesString1 = countriesList.splice(splitIndex).join(" ")
                 var countriesString2 = countriesList.join(" ")
@@ -63,6 +66,17 @@ module.exports = {
                     message.channel.send(countriesString1 + " page 1 of 2");
                 } else if(args[1] === '2') {
                     message.channel.send(countriesString2 + " page 2 of 2");
+                }
+            } else if(args[0] && args[0].toLowerCase() === 'country') {
+                if(!args[1]) {
+                    message.channel.send('you need to specify a country. Try $c19 countries to see the available countries');
+                    return;
+                }
+                for(let x = 0; x < data.length; x++) {
+                    if(data.Countries[x].Slug === args[1]) {
+                        message.channel.send(data.Countries[x]);
+                        return;
+                    }
                 }
             }
         }
