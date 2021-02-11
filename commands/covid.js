3453a0709a23a38972e24bc2a82a6d749f8aa884
Joel Johnson <jjoel1630@gmail.com>
2021-02-11 10:44:13 -0800

    testing covid cmd 4

--- commands/covid.js
+++ commands/covid.js
@@ -52,8 +52,7 @@ module.exports = {
                 .setThumbnail('https://ewscripps.brightspotcdn.com/dims4/default/7671677/2147483647/strip/true/crop/1303x733+15+0/resize/1280x720!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F0a%2Ff2%2F72b1b4d94794992a0772cb593ce5%2Fscreen-shot-2020-02-25-at-10.49.27%20AM.png');
 
                 message.channel.send(Globalembed);
-            }
-            if(args[0] && args[0].toLowerCase() === 'countries') {
+            }else if(args[0] && args[0].toLowerCase() === 'countries') {
                 for(let x = 0; x < data.length; x++) {
                     countries = countries + `\`${data[x].Slug}\``
                 }
