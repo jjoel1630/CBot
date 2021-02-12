586fecc6c254cd814be662139afc12d42b6aba65
Joel Johnson <jjoel1630@gmail.com>
2021-02-12 09:54:26 -0800

    testing about ranperson cmd

---
+++ commands/randomperson.js
@@ -0,0 +1,24 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+
+module.exports = {
+    name: 'randomperson',
+    description: 'randomperson',
+    aliases: ['ranperson', 'rp', 'randomperson'],
+    execute(message=message, args=args, bot=bot) {
+        var request = new XMLHttpRequest();
+
+        var params = '?results=1';
+        var api = 'https://randomuser.me/api/';
+
+        var call = api + params;
+
+        request.open('GET', call, true);
+        request.onload = function () {
+            var data = JSON.parse(this.responseText);
+
+            message.channel.send(data);  
+        }
+        request.send()
+    }
+}
