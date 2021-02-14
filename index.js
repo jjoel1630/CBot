88aa3486080c5424f5c24c6aa3bc5b967f7857a4
Joel Johnson <jjoel1630@gmail.com>
2021-02-13 19:14:11 -0800

    trying out env variables 2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const config = require('./config.json');
 const help = require('./commands/help');
 const aws = require('aws-sdk');
 
-let test = new aws({
+let test = new aws.test({
 	test: process.env.test
 });
 
