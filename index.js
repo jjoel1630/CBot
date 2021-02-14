a2615a911071b0ea2bf18ccb12b35072ccb0eb79
Joel Johnson <jjoel1630@gmail.com>
2021-02-14 11:10:20 -0800

    testing env variables

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ bot.on('ready', async () => {
 });
 
 bot.on('message', message => {
+	console.log(process.env.TEST);
 	if(message.content.toLowerCase() === 'stop the cap' || message.content.toLowerCase() === 'cap') {
 		message.channel.send('https://www.youtube.com/watch?v=mugRenBeRw0&ab_channel=BruhCentralMoments')
 		return;
