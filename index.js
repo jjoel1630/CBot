71b3b4c7c6a47f47f4160ad7bbbe32c827a2d50d
Joel Johnson <jjoel1630@gmail.com>
2021-02-14 15:13:53 -0800

    I set a heroku env variable

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ bot.on('ready', async () => {
 });
 
 bot.on('message', message => {
+	console.log(process.env.token);
 	if(message.content.toLowerCase() === 'stop the cap' || message.content.toLowerCase() === 'cap') {
 		message.channel.send('https://www.youtube.com/watch?v=mugRenBeRw0&ab_channel=BruhCentralMoments')
 		return;
@@ -86,4 +87,4 @@ bot.on("messageDelete", (message) => {
 	deletedMsg.set("deleted msg", {'dcontent': message.content, 'person': message.author.tag, 'created': message.createdAt});
 });
 
-bot.login(config.token);
+bot.login('NzcxNzY5NTI3NDU2ODkwODgw.X5w8Yg.VMXseF-OayyjQP7xIxXWDRiRnQc');
