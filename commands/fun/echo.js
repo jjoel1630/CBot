6d0ed732e7664abc5aab765e6365dc0722f09c56
Joel Johnson <jjoel1630@gmail.com>
2021-02-26 21:52:31 -0800

    added cooldown cmd to the echo cmd

--- commands/fun/echo.js
+++ commands/fun/echo.js
@@ -1,3 +1,8 @@
+const humanizeDuration = require('humanize-duration')
+
+const cooldowns = new Map();
+
+
 module.exports = {
 	name: "Echo",
 	description: "I will echo whatever you tell me to say",
@@ -5,13 +10,23 @@ module.exports = {
 	active: true,
 	perms: 'SEND_MESSAGES', 
 	usage: '`$echo <text>`',
-	execute(message=message, args=args, bot=bot) {
-		if(!args[0]) {
-			message.channel.send("what do you want me to echo idiot???");
-			return;
+	cooldownTime: 3000,
+	execute(message=message, args=args, bot=bot, Discord=Discord) {
+		if(cooldown) {
+			const remaining = humanizeDuration(cooldown - Date.now(), {units: ['m', 's'], round: true});
+			message.channel.send(`chill bruva. you can run this command in remaining`)
+		} else {
+			
+			if(!args[0]) {
+				message.channel.send("what do you want me to echo idiot???");
+				return;
+			}
+			let msgArgs = args.slice(0).join(' ');
+			message.channel.send(msgArgs);
+			message.delete({ timeout: 5 }).catch(console.error);
+
+			cooldowns.set(message.author.id, Date.now() + this.cooldownTime);
+			setTimeout(() => cooldowns.delete(message.author.id), this.cooldownTime);
 		}
-		let msgArgs = args.slice(0).join(' ');
-		message.channel.send(msgArgs);
-		message.delete({ timeout: 5 }).catch(console.error);
 	}
-};
+}
