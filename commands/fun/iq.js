27fa17f67ba6e977d51a10bfaa00fab3349e111c
Joel Johnson <jjoel1630@gmail.com>
2021-02-26 21:55:16 -0800

    added cooldown to the iq cmd

--- commands/fun/iq.js
+++ commands/fun/iq.js
@@ -1,3 +1,8 @@
+const humanizeDuration = require('humanize-duration')
+
+const cooldowns = new Map();
+
+
 module.exports = {
 	name : "IQ", 
 	description : "accurately finds your iq",
@@ -5,35 +10,49 @@ module.exports = {
 	perms: null,
 	active: true,
 	usage: "`$iq <name of person>`",
-	execute(message=message, args=args, bot=bot) {
-		if (message.author.id === '535671100001222668' && !args[0]) {
+	cooldownTime: 30000,
+	execute(message=message, args=args, bot=bot, Discord=Discord) {
+		if(cooldown) {
+			const remaining = humanizeDuration(cooldown - Date.now(), {units: ['m', 's'], round: true});
+			message.channel.send(`chill bruva. you can run this command in remaining`)
+		} else {
+			
+			iq(message, args)
+
+			cooldowns.set(message.author.id, Date.now() + this.cooldownTime);
+			setTimeout(() => cooldowns.delete(message.author.id), this.cooldownTime);
+		}
+	}
+}
+
+const iq = (message, args) => {
+	if (message.author.id === '535671100001222668' && !args[0]) {
+		message.channel.send(`The God's IQ is too high to calculate!`);
+	} else if (!args[0]) {
+		var iq = Math.floor(Math.random() * 300);
+		if (iq === 0) {
+			message.channel.send(
+				`${message.member.user.tag}'s IQ is so low it doesn't register lmaooo`
+			);
+		} else {
+			message.channel.send(`${message.member.user.tag}'s IQ is ${iq}`);
+		}
+	} else if (args[0]) {
+		if (args[0].toLowerCase() === 'joel') {
 			message.channel.send(`The God's IQ is too high to calculate!`);
-		} else if (!args[0]) {
+		} else if(message.mentions.users.first()) {
+			if(message.mentions.users.first().id === '535671100001222668') {
+				message.channel.send(`The God's IQ is too high to calculate!`);
+			}
+		} else {
 			var iq = Math.floor(Math.random() * 300);
 			if (iq === 0) {
 				message.channel.send(
-					`${message.member.user.tag}'s IQ is so low it doesn't register lmaooo`
+					`${args[0]}'s IQ is so low it doesn't register lmaooo`
 				);
 			} else {
-				message.channel.send(`${message.member.user.tag}'s IQ is ${iq}`);
-			}
-		} else if (args[0]) {
-			if (args[0].toLowerCase() === 'joel') {
-				message.channel.send(`The God's IQ is too high to calculate!`);
-			} else if(message.mentions.users.first()) {
-				if(message.mentions.users.first().id === '535671100001222668') {
-					message.channel.send(`The God's IQ is too high to calculate!`);
-				}
-			} else {
-				var iq = Math.floor(Math.random() * 300);
-				if (iq === 0) {
-					message.channel.send(
-						`${args[0]}'s IQ is so low it doesn't register lmaooo`
-					);
-				} else {
-					message.channel.send(`${args[0]}'s IQ is ${iq}`);
-			}
-			}
+				message.channel.send(`${args[0]}'s IQ is ${iq}`);
+		}
 		}
 	}
-};
+}
