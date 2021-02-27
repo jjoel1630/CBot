8e6b92b075def751dc0c6e28bb642acc77191462
Joel Johnson <jjoel1630@gmail.com>
2021-02-26 21:51:54 -0800

    fixed some changes with the cooldown
    made the code into a function so it looked cleaner

--- commands/data/covid.js
+++ commands/data/covid.js
@@ -17,110 +17,118 @@ module.exports = {
 			const remaining = humanizeDuration(cooldown - Date.now(), {units: ['m', 's'], round: true});
 			message.channel.send(`chill bruva. you can run this command in ${remaining}`)
 		} else {
-			var request = new XMLHttpRequest();
-            var api = 'https://api.covid19api.com/summary';
-            var countries = '';
 
-            if(args[0] && args[0] === 'global') {
-                api = 'https://api.covid19api.com/summary'
-            } else if(args[0] && args[0] === 'countries') {
-                api = 'https://api.covid19api.com/countries'
-            } else if(args[0] && args[0].toLowerCase() === 'country') {
-                api = `https://api.covid19api.com/summary`
-            }
+            covid(message, args, Discord);
 
-            request.open('GET', api, true)
-            request.setRequestHeader('X-Access-Token', '5cf9dfd5-3449-485e-b5ae-70a60e997864')
-            request.onload = function () {
-                try {
-                    var data = JSON.parse(this.responseText);
-                } catch(err) {
-                    message.channel.send('There was an error processing your request. Please try again in a few minutes. If you are still facing issues, please contact the owner');
-                }
-
-                if(args[0] && args[0] === 'global') {
-                    const Globalembed = new Discord.MessageEmbed()
-                    .setTitle('Global Covid Summary')
-                    .setTimestamp()
-                    .addFields(
-                        {
-                            name: `NewConfirmed`, value: `${data.Global.NewConfirmed}`
-                        },
-                        {
-                            name: `TotalConfirmed`, value: `${data.Global.TotalConfirmed}`
-                        },
-                        {
-                            name: `NewDeaths`, value: `${data.Global.NewDeaths}`
-                        },
-                        {
-                            name: `TotalDeaths`, value: `${data.Global.TotalDeaths}`
-                        },
-                        {
-                            name: `NewRecovered`, value: `${data.Global.NewRecovered}`
-                        },
-                        {
-                            name: `TotalRecovered`, value: `${data.Global.TotalRecovered}`
-                        },
-                    )
-                    .setColor('#d64545')
-                    .setThumbnail('https://ewscripps.brightspotcdn.com/dims4/default/7671677/2147483647/strip/true/crop/1303x733+15+0/resize/1280x720!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F0a%2Ff2%2F72b1b4d94794992a0772cb593ce5%2Fscreen-shot-2020-02-25-at-10.49.27%20AM.png');
-
-                    message.channel.send(Globalembed);
-                } else if(args[0] && args[0].toLowerCase() === 'countries') {
-                    for(let x = 0; x < data.length; x++) {
-                        countries = countries + `\`${data[x].Slug}\` `
-                    }
-                    var countriesList = countries.split(' ');
-                    countriesList.sort();
-                    var splitIndex = Math.floor(countriesList.length / 2)
-                    var countriesString2 = countriesList.splice(splitIndex).join(" ")
-                    var countriesString1 = countriesList.join(" ")
-
-                    if(!args[1] || args[1] === '1') {
-                        message.channel.send(countriesString1 + " page 1 of 2");
-                    } else if(args[1] === '2') {
-                        message.channel.send(countriesString2 + " page 2 of 2");
-                    }
-                } else if(args[0] && args[0].toLowerCase() === 'country') {
-                    if(!args[1]) {
-                        message.channel.send('you need to specify a country. Try <$c19 countries> to see the available countries');
-                    } else {
-                        for(var x = 0; x < data.Countries.length; x++) {
-                            if(data.Countries[x].Slug === args[1].toLowerCase()) {
-                                const Countrycovidembed = new Discord.MessageEmbed()
-                                .setTitle(`Covid Stats for ${data.Countries[x].Country}`)
-                                .addFields(
-                                    {
-                                        name: `NewConfirmed`, value: `${data.Countries[x].NewConfirmed}`
-                                    },
-                                    {
-                                        name: `TotalConfirmed`, value: `${data.Countries[x].TotalConfirmed}`
-                                    },
-                                    {
-                                        name: `NewDeaths`, value: `${data.Countries[x].NewDeaths}`
-                                    },
-                                    {
-                                        name: `TotalDeaths`, value: `${data.Countries[x].TotalDeaths}`
-                                    },
-                                    {
-                                        name: `NewRecovered`, value: `${data.Countries[x].NewRecovered}`
-                                    },
-                                    {
-                                        name: `TotalRecovered`, value: `${data.Countries[x].TotalRecovered}`
-                                    },
-                                )
-                                .setTimestamp()
-                                .setColor('#d64545')
-                                .setThumbnail('https://ewscripps.brightspotcdn.com/dims4/default/7671677/2147483647/strip/true/crop/1303x733+15+0/resize/1280x720!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F0a%2Ff2%2F72b1b4d94794992a0772cb593ce5%2Fscreen-shot-2020-02-25-at-10.49.27%20AM.png');
-                                message.channel.send(Countrycovidembed);
-                                return;
-                            }
-                        }
-                        message.channel.send('Sorry, we don\'t have data for that country, try <$c19 countries> for the available countries');
-                    }
-                }
-            }
-            request.send();
+			cooldowns.set(message.author.id, Date.now() + this.cooldownTime);
+            setTimeout(() => cooldowns.delete(message.author.id), this.cooldownTime);
 		}
     }
+}
+
+const covid = (message, args, Discord) => {
+    var request = new XMLHttpRequest();
+    var api = 'https://api.covid19api.com/summary';
+    var countries = '';
+
+    if(args[0] && args[0] === 'global') {
+        api = 'https://api.covid19api.com/summary'
+    } else if(args[0] && args[0] === 'countries') {
+        api = 'https://api.covid19api.com/countries'
+    } else if(args[0] && args[0].toLowerCase() === 'country') {
+        api = `https://api.covid19api.com/summary`
+    }
+
+    request.open('GET', api, true)
+    request.setRequestHeader('X-Access-Token', '5cf9dfd5-3449-485e-b5ae-70a60e997864')
+    request.onload = function () {
+        try {
+            var data = JSON.parse(this.responseText);
+        } catch(err) {
+            message.channel.send('There was an error processing your request. Please try again in a few minutes. If you are still facing issues, please contact the owner');
+        }
+
+        if(args[0] && args[0] === 'global') {
+            const Globalembed = new Discord.MessageEmbed()
+            .setTitle('Global Covid Summary')
+            .setTimestamp()
+            .addFields(
+                {
+                    name: `NewConfirmed`, value: `${data.Global.NewConfirmed}`
+                },
+                {
+                    name: `TotalConfirmed`, value: `${data.Global.TotalConfirmed}`
+                },
+                {
+                    name: `NewDeaths`, value: `${data.Global.NewDeaths}`
+                },
+                {
+                    name: `TotalDeaths`, value: `${data.Global.TotalDeaths}`
+                },
+                {
+                    name: `NewRecovered`, value: `${data.Global.NewRecovered}`
+                },
+                {
+                    name: `TotalRecovered`, value: `${data.Global.TotalRecovered}`
+                },
+            )
+            .setColor('#d64545')
+            .setThumbnail('https://ewscripps.brightspotcdn.com/dims4/default/7671677/2147483647/strip/true/crop/1303x733+15+0/resize/1280x720!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F0a%2Ff2%2F72b1b4d94794992a0772cb593ce5%2Fscreen-shot-2020-02-25-at-10.49.27%20AM.png');
+
+            message.channel.send(Globalembed);
+        } else if(args[0] && args[0].toLowerCase() === 'countries') {
+            for(let x = 0; x < data.length; x++) {
+                countries = countries + `\`${data[x].Slug}\` `
+            }
+            var countriesList = countries.split(' ');
+            countriesList.sort();
+            var splitIndex = Math.floor(countriesList.length / 2)
+            var countriesString2 = countriesList.splice(splitIndex).join(" ")
+            var countriesString1 = countriesList.join(" ")
+
+            if(!args[1] || args[1] === '1') {
+                message.channel.send(countriesString1 + " page 1 of 2");
+            } else if(args[1] === '2') {
+                message.channel.send(countriesString2 + " page 2 of 2");
+            }
+        } else if(args[0] && args[0].toLowerCase() === 'country') {
+            if(!args[1]) {
+                message.channel.send('you need to specify a country. Try <$c19 countries> to see the available countries');
+            } else {
+                for(var x = 0; x < data.Countries.length; x++) {
+                    if(data.Countries[x].Slug === args[1].toLowerCase()) {
+                        const Countrycovidembed = new Discord.MessageEmbed()
+                        .setTitle(`Covid Stats for ${data.Countries[x].Country}`)
+                        .addFields(
+                            {
+                                name: `NewConfirmed`, value: `${data.Countries[x].NewConfirmed}`
+                            },
+                            {
+                                name: `TotalConfirmed`, value: `${data.Countries[x].TotalConfirmed}`
+                            },
+                            {
+                                name: `NewDeaths`, value: `${data.Countries[x].NewDeaths}`
+                            },
+                            {
+                                name: `TotalDeaths`, value: `${data.Countries[x].TotalDeaths}`
+                            },
+                            {
+                                name: `NewRecovered`, value: `${data.Countries[x].NewRecovered}`
+                            },
+                            {
+                                name: `TotalRecovered`, value: `${data.Countries[x].TotalRecovered}`
+                            },
+                        )
+                        .setTimestamp()
+                        .setColor('#d64545')
+                        .setThumbnail('https://ewscripps.brightspotcdn.com/dims4/default/7671677/2147483647/strip/true/crop/1303x733+15+0/resize/1280x720!/quality/90/?url=https%3A%2F%2Fewscripps.brightspotcdn.com%2F0a%2Ff2%2F72b1b4d94794992a0772cb593ce5%2Fscreen-shot-2020-02-25-at-10.49.27%20AM.png');
+                        message.channel.send(Countrycovidembed);
+                        return;
+                    }
+                }
+                message.channel.send('Sorry, we don\'t have data for that country, try <$c19 countries> for the available countries');
+            }
+        }
+    }
+    request.send();
 }
--- commands/data/news.js
+++ commands/data/news.js
@@ -13,132 +13,137 @@ module.exports = {
     active: true,
     usage: '`$news everything <keywords>`, `$news top-headlines <country (default: us)> <category (optional)>`, Type $news help for more info`',
     cooldownTime: 120000,
-    execute(message=message, args=args, bot=bot) {
+    execute(message=message, args=args, bot=bot, Discord=Discord) {
         if(cooldown) {
             const remaining = humanizeDuration(cooldown - Date.now(), {units: ['m', 's'], round: true});
             message.channel.send(`chill bruva. you can run this command in remaining`)
         } else {
-            var request = new XMLHttpRequest();
 
-            key = '&apiKey=1b28b79af51a4d28816452334d1dee0f';
-            params = ``;
-            api = ``;
-
-            if(args[0] === 'help') {
-                const Newshelpembed = new Discord.MessageEmbed()
-                .setTitle('Parameters')
-                .addFields(
-                    {
-                        name: `Possible Commands`, value: `\`$news top-headlines <country (default: us)> <category (optional)>\`
-                        \`$news everything <keywords>\``
-                    },
-                    {
-                        name: `Type (Param 1)`, value: `\`top-headlines\` \`everything\``
-                    },
-                    {
-                        name: `Countries (Param 2)`, value: `\`ae\` \`ar\` \`at\` \`au\` \`be\` \`bg\` \`br\` \`eg\` \`fr\` \`gb\` \`gr\` \`hk\` 
-                        \`ca\` \`ch\` \`cn\` \`co\` \`cu\` \`cz\` \`de\` \`kr\` \`lt\` \`lv\` \`ma\` \`mx\`
-                        \`hu\` \`id\` \`ie\` \`il\` \`in\` \`it\` \`jp\` \`pt\` \`ro\` \`rs\` \`ru\` \`sa\`
-                        \`my\` \`ng\` \`nl\` \`no\` \`nz\` \`ph\` \`pl\` \`ua\` \`us\` \`ve\` \`za\`
-                        \`se\` \`sg\` \`si\` \`sk\` \`th\` \`tr\` \`tw\``
-                    },
-                    {
-                        name: `Categories (Param 3)`, value: `\`business\` \`entertainment\` \`general\` \`health\` 
-                        \`science\` \`sports\` \`technology\``
-                    },
-                );
-                message.channel.send(Newshelpembed);
-                return;
-            }
-
-            if(args[0] == 'everything') {
-                if(args[1]) {
-                    key = '&apiKey=1b28b79af51a4d28816452334d1dee0f';
-                    params = `?q=${args.slice(1).join(' ')}`;
-                    api = `https://newsapi.org/v2/${args[0]}`;
-                } else {
-                    message.channel.send('Please provide a keyword to search for your article');
-                }
-            } else if (args[0] == 'top-headlines') {
-                params = `?country=us`;
-                api = `https://newsapi.org/v2/${args[0]}`; 
-                if(args[2]) {
-                    params = `?country=${args[1]}&category=${args[2]}`;
-                    api = `https://newsapi.org/v2/${args[0]}`; 
-                } else if(args[1] && args[1].length > 2) {
-                    params = `?country=us&category=${args[1]}`;
-                    api = `https://newsapi.org/v2/${args[0]}`; 
-                } else if (args[1] && args[1].length <= 2) {
-                    params = `?country=${args[1]}`;
-                    api = `https://newsapi.org/v2/${args[0]}`; 
-                }
-            } else if (!args[0]) {
-                params = `?country=us`;
-                api = `https://newsapi.org/v2/top-headlines`; 
-            } else {
-                message.channel.send('Try $news help');
-                return;
-            }
-
-            call = api + params + key
-
-            console.log(call);
-
-            request.open('GET', call, true)
-            request.onload = function () {
-                try {
-                    var data = JSON.parse(this.responseText);
-
-                    if(!data) {
-                        console.log('try $news help');
-                        return;
-                    }
-
-                    articleNum = Math.floor(Math.random() * 10)
-
-                    var article = JSON.stringify(data.articles[articleNum], null, 4)
-                    var articleSource = JSON.stringify(data.articles[articleNum].source.name, null, 4)
-                    var articleAuthor = JSON.stringify(data.articles[articleNum].author, null, 4)
-                    var articleTitle = JSON.stringify(data.articles[articleNum].title, null, 4)
-                    var articleDescription = JSON.stringify(data.articles[articleNum].description, null, 4)
-                    var articleUrl = JSON.stringify(data.articles[articleNum].url, null, 4)
-                    var articleImage = JSON.stringify(data.articles[articleNum].urlToImage, null, 4)
-                    var articleTime = JSON.stringify(data.articles[articleNum].publishedAt, null, 4)
-                    var articleContent = JSON.stringify(data.articles[articleNum].content, null, 4)
-
-
-                    articleImage = articleImage.slice(0, -1);
-                    articleImage = articleImage.substring(1);
-                    articleUrl = articleUrl.slice(0, -1);
-                    articleUrl = articleUrl.substring(1);
-                    const Newsembed = new Discord.MessageEmbed()
-                    .setTitle(`Article Title: ${articleTitle}`)
-                    .setURL(articleUrl)
-                    .setDescription(`**Description:** ${articleDescription}`)
-                    .setImage(articleImage)
-                    .addFields(
-                        {
-                            name: `From`, value: `${articleSource}`
-                        },
-                        {
-                            name: `Published at:`, value: `${articleTime}`
-                        },
-                        {
-                            name: `By:`, value: `${articleAuthor}`
-                        },
-                        {
-                            name: `Content:`, value: `${articleContent}`
-                        },
-                    )
-                    message.channel.send(Newsembed);
-                } catch(err) {
-                    console.error(err);
-                }
-            }
-            request.send()
+            news(message, args, Discord)
             
             cooldowns.set(message.author.id, Date.now() + this.cooldownTime);
             setTimeout(() => cooldowns.delete(message.author.id), this.cooldownTime);
         }
     }
+}
+
+const news = (message, args, Discord) => {
+    var request = new XMLHttpRequest();
+
+    key = '&apiKey=1b28b79af51a4d28816452334d1dee0f';
+    params = ``;
+    api = ``;
+
+    if(args[0] === 'help') {
+        const Newshelpembed = new Discord.MessageEmbed()
+        .setTitle('Parameters')
+        .addFields(
+            {
+                name: `Possible Commands`, value: `\`$news top-headlines <country (default: us)> <category (optional)>\`
+                \`$news everything <keywords>\``
+            },
+            {
+                name: `Type (Param 1)`, value: `\`top-headlines\` \`everything\``
+            },
+            {
+                name: `Countries (Param 2)`, value: `\`ae\` \`ar\` \`at\` \`au\` \`be\` \`bg\` \`br\` \`eg\` \`fr\` \`gb\` \`gr\` \`hk\` 
+                \`ca\` \`ch\` \`cn\` \`co\` \`cu\` \`cz\` \`de\` \`kr\` \`lt\` \`lv\` \`ma\` \`mx\`
+                \`hu\` \`id\` \`ie\` \`il\` \`in\` \`it\` \`jp\` \`pt\` \`ro\` \`rs\` \`ru\` \`sa\`
+                \`my\` \`ng\` \`nl\` \`no\` \`nz\` \`ph\` \`pl\` \`ua\` \`us\` \`ve\` \`za\`
+                \`se\` \`sg\` \`si\` \`sk\` \`th\` \`tr\` \`tw\``
+            },
+            {
+                name: `Categories (Param 3)`, value: `\`business\` \`entertainment\` \`general\` \`health\` 
+                \`science\` \`sports\` \`technology\``
+            },
+        );
+        message.channel.send(Newshelpembed);
+        return;
+    }
+
+    if(args[0] == 'everything') {
+        if(args[1]) {
+            key = '&apiKey=1b28b79af51a4d28816452334d1dee0f';
+            params = `?q=${args.slice(1).join(' ')}`;
+            api = `https://newsapi.org/v2/${args[0]}`;
+        } else {
+            message.channel.send('Please provide a keyword to search for your article');
+        }
+    } else if (args[0] == 'top-headlines') {
+        params = `?country=us`;
+        api = `https://newsapi.org/v2/${args[0]}`; 
+        if(args[2]) {
+            params = `?country=${args[1]}&category=${args[2]}`;
+            api = `https://newsapi.org/v2/${args[0]}`; 
+        } else if(args[1] && args[1].length > 2) {
+            params = `?country=us&category=${args[1]}`;
+            api = `https://newsapi.org/v2/${args[0]}`; 
+        } else if (args[1] && args[1].length <= 2) {
+            params = `?country=${args[1]}`;
+            api = `https://newsapi.org/v2/${args[0]}`; 
+        }
+    } else if (!args[0]) {
+        params = `?country=us`;
+        api = `https://newsapi.org/v2/top-headlines`; 
+    } else {
+        message.channel.send('Try $news help');
+        return;
+    }
+
+    call = api + params + key
+
+    console.log(call);
+
+    request.open('GET', call, true)
+    request.onload = function () {
+        try {
+            var data = JSON.parse(this.responseText);
+
+            if(!data) {
+                console.log('try $news help');
+                return;
+            }
+
+            articleNum = Math.floor(Math.random() * 10)
+
+            var article = JSON.stringify(data.articles[articleNum], null, 4)
+            var articleSource = JSON.stringify(data.articles[articleNum].source.name, null, 4)
+            var articleAuthor = JSON.stringify(data.articles[articleNum].author, null, 4)
+            var articleTitle = JSON.stringify(data.articles[articleNum].title, null, 4)
+            var articleDescription = JSON.stringify(data.articles[articleNum].description, null, 4)
+            var articleUrl = JSON.stringify(data.articles[articleNum].url, null, 4)
+            var articleImage = JSON.stringify(data.articles[articleNum].urlToImage, null, 4)
+            var articleTime = JSON.stringify(data.articles[articleNum].publishedAt, null, 4)
+            var articleContent = JSON.stringify(data.articles[articleNum].content, null, 4)
+
+
+            articleImage = articleImage.slice(0, -1);
+            articleImage = articleImage.substring(1);
+            articleUrl = articleUrl.slice(0, -1);
+            articleUrl = articleUrl.substring(1);
+            const Newsembed = new Discord.MessageEmbed()
+            .setTitle(`Article Title: ${articleTitle}`)
+            .setURL(articleUrl)
+            .setDescription(`**Description:** ${articleDescription}`)
+            .setImage(articleImage)
+            .addFields(
+                {
+                    name: `From`, value: `${articleSource}`
+                },
+                {
+                    name: `Published at:`, value: `${articleTime}`
+                },
+                {
+                    name: `By:`, value: `${articleAuthor}`
+                },
+                {
+                    name: `Content:`, value: `${articleContent}`
+                },
+            )
+            message.channel.send(Newsembed);
+        } catch(err) {
+            console.error(err);
+        }
+    }
+    request.send()
 }
