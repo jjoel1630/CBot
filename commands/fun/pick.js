60705618b9d6d03b15039f7d21c9161dc8dee86e
Joel Johnson <jjoel1630@gmail.com>
2021-02-26 21:56:51 -0800

    added cooldown to the pick command

--- commands/fun/pick.js
+++ commands/fun/pick.js
@@ -1,3 +1,8 @@
+const humanizeDuration = require('humanize-duration')
+
+const cooldowns = new Map();
+
+
 module.exports = {
 	name : "Pick", 
 	description : "picks between multiple options SEPARATED BY A SPACE",
@@ -5,14 +10,24 @@ module.exports = {
 	perms: null, 
 	active: true,
 	usage: "`$pick <option 1> <option 2> ...`",
+	cooldownTime: 30000,
 	execute(message=message, args=args, bot=bot, Discord=Discord) {
-		var index = Math.floor(Math.random() * args.length);
-		args.join(', ')
-		const pickEmbed = new Discord.MessageEmbed()
-			.setTitle('Options: ' + args)
-			.addField('Result: ' + args[index], 'That was the result')
-			.setThumbnail(message.author.avatarURL());
-		message.channel.send(pickEmbed);
-		message.delete({ timeout: 100 }).catch(console.error);
+		if(cooldown) {
+			const remaining = humanizeDuration(cooldown - Date.now(), {units: ['m', 's'], round: true});
+			message.channel.send(`chill bruva. you can run this command in remaining`)
+		} else {
+			
+			var index = Math.floor(Math.random() * args.length);
+			args.join(', ')
+			const pickEmbed = new Discord.MessageEmbed()
+				.setTitle('Options: ' + args)
+				.addField('Result: ' + args[index], 'That was the result')
+				.setThumbnail(message.author.avatarURL());
+			message.channel.send(pickEmbed);
+			message.delete({ timeout: 100 }).catch(console.error);
+
+			cooldowns.set(message.author.id, Date.now() + this.cooldownTime);
+			setTimeout(() => cooldowns.delete(message.author.id), this.cooldownTime);
+		}
 	}
-};
+}
