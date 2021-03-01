26cbd75e344374063a60f0c120f4001f4d7cb502
Joel Johnson <jjoel1630@gmail.com>
2021-03-01 10:15:55 -0800

    fixed kick and ban commands

--- commands/mod/ban.js
+++ commands/mod/ban.js
@@ -9,7 +9,7 @@ module.exports = {
 	aliases: ["ban"],
 	active: true,
 	perms: ['KICK_MEMBERS'],
-	usage: "`$ban <@user>`",
+	usage: "`$ban <@user> <time (in days)> <reason>`",
 	cooldownTime: 10000,
 	execute(message=message, args=args, bot=bot, Discord=Discord) {
 		const cooldown = cooldowns.get(message.author.id);
@@ -28,29 +28,45 @@ module.exports = {
 
 
 const ban = (message, args) => {
+	if(!args[2]) {
+		message.channel.send(this.usage);
+	}
+
+	const time = args[1];
+	args.shift();
+	args.shift();
+	const reason = args.join(" ");
 	const { member, mentions } = message;
+
+	if(!reason || !time) {
+		message.channel.send(this.usage);
+		return;
+	}
+	
 	if (member.hasPermission('ADMINISTRATOR') || member.hasPermission('BAN_MEMBERS')) {
 		const target = mentions.users.first();
 
-		if(target.id === '814565277480124427') {
+		if(target?.id === '771769527456890880') {
+			message.channel.send('u cant ban me dumbo stop trying');
+			return;
+		} else if(target?.id === '535671100001222668') {
 			message.channel.send('u cant ban my creator dumbo stop trying');
+			return;
+		} else if(target?.id === message.member.id) {
+			message.channel.send('you cannot ban yourself idiot');
 			return;
 		}
 
 		if (target) {
-			try {
-				const targetMember = message.guild.members.cache.get(target.id);
-				targetMember.ban().then(() => {
-					message.channel.send(
-						`<@${targetMember.id}>, has been banned lmaoooooo. Get yo butt outta here.`
-					);
-				}).catch(err => {
-					message.channel.send(`stop tryna ban people that have a higher role than u. hey admins ban <@${message.author.id}>`);
-					return;
-				})
-			} catch(err) {
-				message.channel.send(`stop tryna ban people that have a high role than u. hey admins ban <@${target.id}>`);
-			}
+			const targetMember = message.guild.members.cache.get(target.id);
+			targetMember.ban({ days: time, reason: reason }).then(() => {
+				message.channel.send(
+					`<@${targetMember.id}>, has been banned lmaoooooo. Get yo butt outta here.`
+				);
+			}).catch(err => {
+				message.channel.send(`<@${targetMember.id}> has a higher role than me dumbo`);
+				return;
+			});
 		} else {
 			message.channel.send(
 				`<@${member.id}>, bro you gotta tell me who to ban stupid. SMH.`
--- commands/mod/kick.js
+++ commands/mod/kick.js
@@ -4,7 +4,7 @@ module.exports = {
 	aliases: ["kick"],
 	perms: ["BAN_MEMBERS"],
 	active: false,
-	usage: "`$kick <@user>`",
+	usage: "`$kick <@user> <reason>`",
 	execute(message=message, args=args, bot=bot) {
 		
 	}
@@ -39,31 +39,47 @@ module.exports = {
 }
 
 const kick = (message, args) => {
+	if(!args[1]) {
+		message.channel.send(this.usage);
+	}
+
+	args.shift();
+	const reason = args.join(" ");
 	const { member, mentions } = message;
-	if (member.hasPermission('ADMINISTRATOR') || member.hasPermission('KICK_MEMBERS')) {
+
+	if(!reason) {
+		message.channel.send(this.usage);
+		return;
+	}
+	
+	if (member.hasPermission('ADMINISTRATOR') || member.hasPermission('BAN_MEMBERS')) {
 		const target = mentions.users.first();
 
-		if(target.id === '814565277480124427') {
+		if(target?.id === '771769527456890880') {
+			message.channel.send('u cant kick me dumbo stop trying');
+			return;
+		} else if(target?.id === '535671100001222668') {
 			message.channel.send('u cant kick my creator dumbo stop trying');
+			return;
+		} else if(target?.id === message.member.id) {
+			message.channel.send('you cannot kick yourself idiot');
 			return;
 		}
 
 		if (target) {
-			try {
-				const targetMember = message.guild.members.cache.get(target.id);
-				targetMember.kick().then(() => {
-					message.channel.send(
-						`<@${targetMember.id}>, has been banned lmaoooooo. Get yo butt outta here.`
-					);
-				}).catch(err => {
-					message.channel.send(`stop tryna kick people that have a higher role than u. hey admins kick <@${message.author.id}>`);
-					return;
-				})
-			} catch(err) {
-				message.channel.send(`stop tryna kick people that have a high role than u. hey admins kick <@${target.id}>`);
-			}
+			const targetMember = message.guild.members.cache.get(target.id);
+			targetMember.kick({ reason: reason }).then(() => {
+				message.channel.send(
+					`<@${targetMember.id}>, has been kicked lmaoooooo. Get yo butt outta here.`
+				);
+			}).catch(err => {
+				message.channel.send(`<@${targetMember.id}> has a higher role than me dumbo`);
+				return;
+			});
 		} else {
-			message.channel.send(`<@${member.id}>, bro you gotta tell me who to kick stupid. SMH.`);
+			message.channel.send(
+				`<@${member.id}>, bro you gotta tell me who to kick stupid. SMH.`
+			);
 		}
 	} else {
 		message.channel.send("Bro you don't even have perms. You cant just kick people man.");
