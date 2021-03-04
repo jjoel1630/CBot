7321b4d210605b426b8f03b0f2aaded0f89effd3
Joel Johnson <jjoel1630@gmail.com>
2021-03-03 16:11:23 -0800

    testing quick.db

--- newtest.js
+++
@@ -1,7 +0,0 @@
-const db = require('quick.db');
-
-module.exports = () => {
-    var economy = new db.table('economy')
-    economy.set('myNewBalance', 800) // -> 500
-    console.log(economy.get('myBalance')) // -> 500
-}
