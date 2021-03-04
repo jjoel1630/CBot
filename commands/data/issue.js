d0eb0f9c03f1f3d559c84f6cd9479790d3b604ed
Joel Johnson <jjoel1630@gmail.com>
2021-03-04 14:18:09 -0800

    finished the issue command

--- commands/data/issue.js
+++ commands/data/issue.js
@@ -16,7 +16,7 @@ module.exports = {
         const cooldown = cooldowns.get(message.author.id);
         if(cooldown && message.author.id !== '535671100001222668') {
             const remaining = humanizeDuration(cooldown - Date.now(), {units: ['m', 's'], round: true});
-            message.channel.send(`chill bruva like my bot aint that bad that u need to spam issues. right.......? :(. you can run this command in ` + remaining)
+            message.channel.send(`chill bruva. my bot aint that bad that u need to spam issues. right.......? :(. you can run this command in ` + remaining)
         } else {
             
             issues(message, args);
@@ -30,24 +30,39 @@ module.exports = {
 const issues = (message, args) => {
     var request = new XMLHttpRequest();
 
+    let i = 0;
+    let spliceIndex = undefined;
+    args.forEach(arg => {
+        if(arg.endsWith(',')) {
+            args[i] = arg.slice(0, -1);
+            spliceIndex = i;
+        }
+        i++
+    });
+    if(spliceIndex === undefined) {
+        message.channel.send('u need to separate ure title and description with a comma smart one');
+        return;
+    }
+    const argsDescription = args.splice(spliceIndex + 1);
+    const  description = argsDescription.join(' ');
+    const title = args.join(' ');
+
     var call = 'https://api.github.com/repos/jjoel1630/CBot/issues';
     var bodyParams = {
-        "title": "this is a title",
-        "body": "this is a description"
+        "title": title,
+        "body": description
     }
 
-    message.channel.send('sent request');
-
-    // request.open('POST', call, true);
-    // request.setRequestHeader('Content-Type', 'application/json');
-    // request.setRequestHeader('Authorization', `token ${process.env.GITHUB_TOKEN ?? process.env.H_GITHUB_TOKEN}`);
-    // request.onload = function () {
-    //     try {
-    //         message.channel.send('new issue created');
-    //     } catch(err) {
-    //         console.log(err);
-    //         message.channel.send('There was an error sending your issue');
-    //     }
-    // }
-    // request.send(JSON.stringify(bodyParams));
+    request.open('POST', call, true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.setRequestHeader('Authorization', `token ${process.env.GITHUB_TOKEN ?? process.env.H_GITHUB_TOKEN}`);
+    request.onload = function () {
+        try {
+            message.channel.send('new issue created');
+        } catch(err) {
+            console.log(err);
+            message.channel.send('There was an error creating your issue');
+        }
+    }
+    request.send(JSON.stringify(bodyParams));
 }
