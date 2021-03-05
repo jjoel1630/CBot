1e996fdb192f31429505a5b84e89e1551c39fc01
Joel Johnson <jjoel1630@gmail.com>
2021-03-05 08:29:26 -0800

    finished math command

--- commands/data/math.js
+++ commands/data/math.js
@@ -1,5 +1,3 @@
-const functions = require('../../mathfunctions.js');
-
 module.exports = {
     name: 'Calculator',
     description: 'calculates various operations in math',
@@ -43,68 +41,105 @@ module.exports = {
             .setColor('#5AADFF')
 
             message.channel.send(mathHelp);
+            return;
+        }
+
+        if(args[1] && !parseFloat(args[1])) {
+            message.channel.send('you gotta gimme a valid number dumbo');
+            return;
+        }
+
+        if(args[2] && !parseFloat(args[2])) {
+            message.channel.send('you gotta gimme a valid number dumbo');
+            return;
         }
 
         switch(args[0].toLowerCase()) {
-            case 'cuberoot' || 'cbrt':
+            case 'cuberoot':
+            case 'cbrt':
+                console.log('cbrt');
                 if(!args[1]) {
                     message.channel.send('try $math help');
                     break;
                 }
-                functions.cubeRoot(args[1]);
+                message.channel.send(cubeRoot(args[1]));
                 break;
-            case 'squareroot' || 'sqrt':
+            case 'squareroot':
+            case 'sqrt':
                 if(!args[1]) {
                     message.channel.send('try $math help');
                     break;
                 }
-                functions.cubeRoot(args[1]);
+                message.channel.send(squareRoot(args[1]));
                 break;
-            case 'power':
-                if(!args[2]) {
-                    message.channel.send('try $math help');
-                    break;
-                }
-                functions.power(args[1], args[2])
-                break;
-            case 'circum' || 'circumference':
+            case 'circum':
+            case 'circumference':
                 if(!args[1]) {
                     message.channel.send('try $math help');
                     break;
                 }
-                functions.calculateCircumference(args[1]);
+                message.channel.send(calculateCircumference(args[1]));
                 break;
             case 'degtorad':
                 if(!args[1]) {
                     message.channel.send('try $math help');
                     break;
                 }
-                functions.degToRad(args[1]);
+                message.channel.send(degToRad(args[1]));
                 break;
             case 'radtodeg':
                 if(!args[1]) {
                     message.channel.send('try $math help');
                     break;
                 }
-                functions.radToDeg(args[1]);
+                message.channel.send(radToDeg(args[1]));
                 break;
             case 'apothem':
                 if(!args[1]) {
                     message.channel.send('try $math help');
                     break;
                 }
-                functions.apothem(args[1]);
+                message.channel.send(apothem(args[1]));
                 break;
-            case 'power' || 'pow':
+            case 'power':
+            case 'pow':
                 if(!args[2]) {
                     message.channel.send('try $math help');
                     break;
                 }
-                functions.power(args[1], args[2]);
+                message.channel.send(power(args[1], args[1]));
                 break;
             default:
-                message.channel.send('what operation do you want me to perform smart one??? no wonder you need a calculator lmao.');
+                message.channel.send('what operation do you want me to perform smart one??? no wonder you need a calculator lmao. (get it. cuz ure that dumb. NVM)');
                 break;
         }
     }
+}
+
+const calculateCircumference = (radius) => {
+    return 2 * Math.PI * radius;
+}
+
+const degToRad = (deg) => {
+    return deg * (Math.PI / 180);
+}
+
+const radToDeg = (rad) => {
+    return rad / (Math.PI / 180);
+}
+
+const apothem = (deg) => {
+    return 50 * Math.tan(this.degToRag(deg));
+}
+
+const cubeRoot = (number) => {
+    return Math.cbrt(number);
+}
+
+const squareRoot = (number) => {
+    return Math.sqrt(number);
+}
+
+const power = (x, y) => {
+    return Math.pow(x, y);
 }
